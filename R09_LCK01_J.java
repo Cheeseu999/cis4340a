28c350a80610946fcae30260966514d0473d8e65
Cheeseu999 <avanaf942@gmail.com>
2025-02-28 23:49:54 -0500

    Update R09_LCK01_J.java
    Compliant example.

--- R09_LCK01_J.java
+++ R09_LCK01_J.java
@@ -1,5 +1,5 @@
 private int count = 0;
-private final Integer Lock = count; // Boxed primitive Lock is shared
+private final Integer Lock = new Integer(count); // Boxed primitive Lock is shared
  
 public void doSomething() {
   synchronized (Lock) {
